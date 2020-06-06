8b0a9f92c78f7343108710665f8900881c5c51a6
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-06-06 21:58:34 +0530

    functions in php

---
+++ function/f1.php
@@ -0,0 +1,7 @@
+<?php
+    echo strlen("Flowers"); // characters in string
+    echo str_word_count("hello everyone "); //words in string
+    echo strrev("Phone");
+    echo strpos("hi Jone","J");// possision
+    echo str_replace("jone","Jeni","Hi jone");
+?> 
