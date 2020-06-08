ab72c55901458b41ce2253bcd274be1a8ae6a54c
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-06-08 22:01:05 +0530

    DataTypes in PHP

---
+++ BasicPHP/data_types.php
@@ -0,0 +1,15 @@
+<?php
+//String
+     $name = "Coding is fun";
+     $name1 ='coding is fun';
+//Integer
+    $name = 20;
+//float
+    $name = 20.56;
+//boolean
+   // true = 1
+    //false = 0
+//Arrays
+    $names = array("Daniel","Dennis","Michael");
+    echo $names['0'];
+?>
