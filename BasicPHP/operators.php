d48a1b77f6f847906574a5b6c862909c278d1271
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-06-08 22:50:03 +0530

    Operators in PHP

---
+++ BasicPHP/operators.php
@@ -0,0 +1,46 @@
+<?php
+//Arithmetic Operators 
+    echo 5+5;
+    echo 10-5;
+    echo 10/5;
+    echo 5*5;
+    echo 8%5;
+    echo 5**5;
+
+// Assignment Operators
+    $x = 100;
+    $x +=20;
+    echo $x;
+
+    $Y = 100;
+    $Y /= 20;
+    echo $Y;
+
+//Comparison Operators
+    $Y = 5;
+    $x = 10;
+
+    if ($x == $Y){
+        echo "True!";
+    }else{
+        echo "False!";
+    }
+    /* != not equel
+       === equel and same datatype */
+
+//Logical Operator
+    $Y=10;
+    $X=20;
+
+    if($X==$Y or 1==1){
+        echo "True";
+    
+        // or = ||  and = &&  
+    }
+
+// Increment, Decrement Operators
+    $z = 10;
+    echo ++$z;
+    echo --$z;
+    
+?>
