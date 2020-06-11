8f1e8ef0c0cb340d56b6e7df56f99fd65f1f66e0
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-06-11 14:13:32 +0530

    if else statement in php

---
+++ conditional_statement/if_else.php
@@ -0,0 +1,21 @@
+<?php
+    $x = 4;
+
+    if ($x == 1){
+        echo "Incorrect index";
+    } else{
+        echo "correct index";
+    }
+
+// elseif
+    $y = 2;
+    if ($y == 1){
+        echo "Incorrect index";
+    }
+    elseif($y == 2){
+        echo "undefind index";
+    } 
+    else{
+        echo "correct index";
+    }
+?>
