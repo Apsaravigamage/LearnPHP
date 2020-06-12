791712f5b8fe361dfd127c099d82271c793265a9
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-06-12 17:13:26 +0530

    Swithch statement in php

---
+++ conditional_statement/switch.php
@@ -0,0 +1,19 @@
+<?php
+    $x = 38;
+
+    switch($x) {
+        case 1:
+            echo"The answer is 1";
+        break;
+
+        case 2:
+            echo"The answer is 2";
+        break; 
+
+        case 3:
+            echo"The answer is 3";
+        break;
+        default:
+            echo"There is no answer";
+    }
+?>
