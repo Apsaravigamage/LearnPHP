0165fd54b62af91667d76775a41c0fee4631b8a6
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-06-23 10:37:50 +0530

    session and cookic in PHP

---
+++ session&cookies.php
@@ -0,0 +1,8 @@
+<?php
+    //cookic saving infoemation in user side 
+    // session saving infomation in server side
+
+    setcookie("name","Daniel",time() - 86400);
+    $_SESSION['name']="12 ";
+    
+?>
